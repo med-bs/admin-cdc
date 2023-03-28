e28aa87d9c111a8886415f05ffda6c807c80ef45
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-03-28 12:52:04 +0100

    team content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./scenes/Dashboard";
 import SideBar from "./scenes/global/SideBar";
 import TopBar from "./scenes/global/TopBar";
+import Team from "./scenes/Team.jsx";
 import { ColorModeContext, useMode } from "./theme";
 
 function App() {
@@ -11,14 +12,15 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <div className="app">
-          <SideBar/>
-        <CssBaseline />
-        <main className="content">
-          <TopBar />
-          <Routes>
-            <Route path="/" element={<Dashboard/>}/>
-          </Routes>
-        </main>
+          <SideBar />
+          <CssBaseline />
+          <main className="content">
+            <TopBar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+            </Routes>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
