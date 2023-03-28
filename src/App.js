0a6d61c7a426d9cce8da3511089fc3486b250258
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-03-28 11:31:37 +0100

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./scenes/Dashboard";
+import SideBar from "./scenes/global/SideBar";
 import TopBar from "./scenes/global/TopBar";
 import { ColorModeContext, useMode } from "./theme";
 
@@ -7,10 +10,16 @@ function App() {
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
+        <div className="app">
+          <SideBar/>
         <CssBaseline />
         <main className="content">
           <TopBar />
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+          </Routes>
         </main>
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
 
