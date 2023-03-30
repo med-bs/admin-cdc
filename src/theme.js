450fb4fe00e97c05413b804bee3696932193a963
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-03-30 16:32:02 +0100

    theme stable v1.0

--- src/theme.js
+++ src/theme.js
@@ -201,7 +201,6 @@ export const ColorModeContext = createContext({
 
 export const useMode = () => {
   const [mode, setMode] = useLocalStorage('mode' ? 'dark' : 'light')
-
   const colorMode = useMemo(
     () => ({
       toggleColorMode: () =>
