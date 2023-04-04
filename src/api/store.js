877cf27f15f5a999d6fef744e991987b6be8e6f8
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-04-04 23:08:33 +0100

    customer api

--- src/api/store.js
+++ src/api/store.js
@@ -2,11 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import connectorReducer from "./cdc/connectorSlice";
 import kafkaReducer from "./cdc/kafkaSlice"
+import customerReducer from "./bank/customerSlice"
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+
         cdc: connectorReducer,
-        kafka: kafkaReducer
+        kafka: kafkaReducer,
+
+        customers:customerReducer,
     },
 });
