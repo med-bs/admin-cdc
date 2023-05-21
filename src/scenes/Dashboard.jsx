c83dfc00a6a69e37c443ea2861b1cdb0890aae1f
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-05-21 23:24:11 +0100

    save url in .env file

--- src/scenes/Dashboard.jsx
+++ src/scenes/Dashboard.jsx
@@ -1,12 +1,16 @@
 const Dashboard = () => {
+
+    const kibanaURL = process.env.REACT_APP_KIBANA_URL;
+
     return (
         <iframe
-            src="http://localhost:5601/goto/772c5cb0-f4d9-11ed-acb8-bbd2be5a62ef"
-            width="100%"
-            height="90%"
-            title="Kibana Dashboard"
+          src={kibanaURL}
+          width="100%"
+          height="90%"
+          title="Kibana Dashboard"
         ></iframe>
-    );
-};
 
-export default Dashboard;
+    );
+  };
+  
+  export default Dashboard;
--- src/scenes/cdc/Connectors.jsx
+++ src/scenes/cdc/Connectors.jsx
@@ -1,15 +1,13 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import Header from "../../components/Header";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { getConnectorStatus, getConnectors, reset, runConnector, stopConnector } from "../../api/cdc/connectorSlice";
+import { getConnectors, reset } from "../../api/cdc/connectorSlice";
 import { tokens } from "../../theme";
 
 import { IconButton } from '@mui/material';
-import PlayCircleOutlineOutlinedIcon from '@mui/icons-material/PlayCircleOutlineOutlined';
-import StopCircleOutlinedIcon from '@mui/icons-material/StopCircleOutlined';
-import AutorenewIcon from '@mui/icons-material/Autorenew';
+
 import VerifiedOutlinedIcon from '@mui/icons-material/VerifiedOutlined';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
@@ -29,22 +27,6 @@ const Connectors = () => {
     const { kafkaConnectors, kafkaIsLoading, kafkaIsError, kafkaMessage } = useSelector(
         (state) => state.kafka
     );
-
-    const onRefrech = (name) => {
-        dispatch(getConnectorStatus(name));
-    }
-
-    const onPlay = (name, table) => {
-        const connectorData = {
-            containerName: name,
-            topicName: table
-        }
-        dispatch(runConnector(connectorData));
-    }
-
-    const onStop = (name) => {
-        dispatch(stopConnector(name));
-    }
 
     const onAdd = (data) => {
         dispatch(createKafkaConnector(data));
@@ -74,68 +56,7 @@ const Connectors = () => {
             { field: "name", headerName: "Name", flex: 1 },
             { field: "kafka_topic", headerName: "Kafka Topic", flex: 1 },
             {
-                field: "status",
-                headerName: "Status",
-                headerAlign: "center",
-                flex: 1,
-                renderCell: ({ row: { status } }) => {
-                    return (
-                        <Box
-
-                            width="60%"
-                            m="0 auto"
-                            p="5px"
-                            display="flex"
-                            justifyContent="center"
-                            backgroundColor={
-                                status === "not found"
-                                    ? colors.redAccent[700]
-                                    : status === "running"
-                                        ? colors.greenAccent[700]
-                                        : colors.blueAccent[700]
-                            }
-                            borderRadius="4px"
-                        >
-                            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
-                                {status}
-                            </Typography>
-                        </Box>
-                    );
-                },
-            },
-            {
-                field: "name1",
-                headerName: "Start Connector",
-                headerAlign: "center",
-                flex: 1,
-                renderCell: (row) => {
-                    return (
-                        <Box
-                            width="80%"
-                            m="0 auto"
-                            p="5px"
-                            display="flex"
-                            justifyContent="center"
-                            backgroundColor={
-                                row.row.status === "not found"
-                                    ? colors.redAccent[700]
-                                    : row.row.status === "running"
-                                        ? colors.greenAccent[700]
-                                        : colors.blueAccent[700]
-                            }
-                            borderRadius="4px"
-                        >
-                            <IconButton onClick={() => onPlay(row.row.name, row.row.kafka_topic)} disabled /* ={row.row.status === "running"} */><PlayCircleOutlineOutlinedIcon color={colors.grey[100]} /></IconButton>
-                            <IconButton onClick={() => onStop(row.row.name)} disabled /* ={row.row.status !== "running"} */><StopCircleOutlinedIcon color={colors.grey[100]} /></IconButton>
-                            <IconButton onClick={() => onRefrech(row.row.name)} disabled ><AutorenewIcon color={colors.grey[100]} /></IconButton>
-
-                        </Box >
-                    );
-                },
-            },
-
-            {
-                field: "name2",
+                field: "kafka_connect",
                 headerName: "Kafka Connect",
                 headerAlign: "center",
                 flex: 1,
